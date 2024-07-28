ded958f95ad393a62b1ffe25725e7995e1d4d6f0
Nicholas <njbroussard.1@gmail.com>
2024-07-28 10:05:28 -0500

    install express, get persons function

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const http = require('http')
+const express = require('express')
+app = express();
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+
+let persons = [
+  {
+    id: "1",
+    name: "Nicholas Broussard",
+    number: "504-919-3191"
+  },
+  {
+    id: "2",
+    name: "Albert Brooks",
+    number: "504-919-3192"
+  },
+  {
+    id: "3",
+    name: "Louis CK",
+    number: "504-919-3193"
+  },
+  {
+    id: "4",
+    name: "Frank Sinatra",
+    number: "504-919-3194"
+  }
+]
+
+
+app.get('/', (request, response) => {
+  response.send(`<h1>Hello World!</h1>`)
 })
 
-//console.log('hello world')
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
 
 const PORT = 3001
 app.listen(PORT)
